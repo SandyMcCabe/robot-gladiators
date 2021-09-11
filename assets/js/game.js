5810e317ceed6832cd7f1860515df227231a4878
Sandy McCabe <sandytech13@gmail.com>
2021-09-10 22:15:39 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -93,6 +93,11 @@ var endGame = function(){
   }
 };
 
+var shop = function () {
+  console.log("entered the shop");
+
+}
+
 var startGame = function() {
 // reset player stats
 playerHealth = 100;
@@ -117,6 +122,11 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+
+    // if we're not at the last enemy in the array
+    if(playerHealth > 0 && i < enemyNames.length -1) {
+      shop();
+    }
   }
   // 
   endGame();
